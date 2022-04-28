9a70217d606f3113c73eb5d359196b1241c763bb
Mathis-COCO <mathis.coco@ynov.com>
2022-04-28 15:40:27 +0200

    first push

---
+++ MVC/App/Models/Race.cs
@@ -0,0 +1,14 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace App.Models
+{
+    public class Race
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime EventDate { get; set; }
+        public int MaxPlaces { get;set; }
+
+    }
+}
---
+++ MVC/App/ViewModels/CreateRaceRequest.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace App.ViewModels
+{
+    public class CreateRaceRequest
+    {
+        [Required(ErrorMessage = "Vous devez renseigner un nom à votre course")]
+        [MaxLength(32)]
+        public string? RaceName { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime RaceDate { get; set; }
+
+        [Required(ErrorMessage = "Veuillez préciser le nombre de places")]
+        public int RacePlaces { get; set; }
+    }
+}
